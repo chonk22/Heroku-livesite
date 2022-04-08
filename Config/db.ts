decd13566636c500012635c600a6d953f49eeca5
logan morris <logan-morr@hotmail.ca>
2022-04-08 17:41:22 -0400

    added ICE 12

--- Config/db.ts
+++
@@ -1,4 +0,0 @@
-export const LocalURI="mongodb://127.0.0.1:27017/contacts";
-export const RemoteURI="mongodb+srv://chonk:I2OdZjcydIo6Bq4v@cluster0.y3gqs.mongodb.net/contacts?retryWrites=true&w=majority";
-export const SessionSecret = "WEBD6201SessionSecret";
-export const Host = "Mongo DB Atlas";
---
+++ Server/Config/db.ts
@@ -0,0 +1,5 @@
+export const LocalURI="mongodb://127.0.0.1:27017/contacts";
+export const RemoteURI="mongodb+srv://chonk:I2OdZjcydIo6Bq4v@cluster0.y3gqs.mongodb.net/contacts?retryWrites=true&w=majority";
+export const SessionSecret = "WEBD6201SessionSecret";
+// export const Host = "localhost";
+export const Host = "MongoDB Atlas";
